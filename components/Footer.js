f50a833b8e8f31f4734ba6714376cae996258891
chambersgentile94-art <chambers.gentile94@gmail.com>
2025-12-01 22:51:56 -0300

    Remove unnecessary code comments in Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,3 @@
-```jsx
 export default function Footer(){
   return (
     <footer className="mt-8 bg-white p-4 text-center text-sm text-gray-500">
@@ -6,4 +5,3 @@ export default function Footer(){
     </footer>
   )
 }
-```
