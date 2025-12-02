37c772de4c5892452c416c0a8c441be4fee5c148
chambersgentile94-art <chambers.gentile94@gmail.com>
2025-12-01 22:39:41 -0300

    Add Footer component with copyright notice

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+```jsx
+export default function Footer(){
+  return (
+    <footer className="mt-8 bg-white p-4 text-center text-sm text-gray-500">
+      © {new Date().getFullYear()} TuNombre - Instalaciones eléctricas y cableado estructurado.
+    </footer>
+  )
+}
+```
